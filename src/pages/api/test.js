b9e60cdb6de2035499c5f5eb239ad9f6360e49b4
Bryan Robinson <bryanlrobinson@gmail.com>
2024-11-20 10:45:31 -0500

    Removes fake api endpoint

--- src/pages/api/test.js
+++
@@ -1,9 +0,0 @@
-export const prerender = false;
-
-export async function GET({ params }) {
-    return new Response(
-        JSON.stringify({ message: "Hello, world!" }),
-       {
-        status: 200,
-       }
-    )};
