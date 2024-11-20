4fdca8f0dc241d37c47870e5d470904acd17c878
Bryan Robinson <bryanlrobinson@gmail.com>
2024-11-20 10:02:06 -0500

    First commit

---
+++ src/utils/bluesky.ts
@@ -0,0 +1,12 @@
+import { AtpAgent } from '@atproto/api'
+
+const agent = new AtpAgent({
+  service: 'https://bsky.social'
+})
+
+await agent.login({
+    identifier: 'brob.dev',
+    password: import.meta.env.BLUESKY_PASSWORD
+  })
+
+export default agent
